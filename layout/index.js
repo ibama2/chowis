77d99998f0294c9fb28cc2499037eca7f503737c
ibama2 <treeofstar@gmail.com>
2023-07-05 17:54:22 +0800

    Update index.js

--- layout/index.js
+++ layout/index.js
@@ -7,104 +7,215 @@ export default function Layout({ children }) {
   const router = useRouter();
   const goHome = () => router.push('/');
 
-  const path = (id) => {
-    switch(id) {
-      case '0':
-        return '/';
-      case '7':
-        return '/contact';
-      case '8':
-        return '/policy';
-      default:
-        return `/products/${id}`
-    }
-  }
-
   return (
     <div className={styles.container}>
       <Head>
         <title>昭盈偲</title>
         <link rel="icon" href="/images/headIcon.png" />
       </Head>
+      <nav className="navbar">
+        <img onClick={goHome} src="/images/chowislogo.webp" fetchpriority="high" />
+
+        <ul className="nav-links">
+
+          <input type="checkbox" id="checkbox_toggle" />
+          <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
+
+          <div className="menu">
+            <li><Link href='/'>首页</Link></li>
+            <li className="products">
+              <Link href='#'>产品</Link>
+              
+              <ul className="dropdown">
+                {[
+                  { id: '1', name: 'DermoPrime' },
+                  { id: '2', name: 'DermoSmart' },
+                  { id: '3', name: 'DermoPico' },
+                  { id: '4', name: 'mySkin·Hair KIOSK' },
+                  { id: '5', name: 'mySkin FAIN' },
+                  { id: '6', name: 'DermoChoice' }
+                ].map(product => (
+                  <li key={product.id}><Link href={`/products/${product.id}`}>{product.name}</Link></li>
+                ))}
+              </ul>
+            </li>
+            <li><Link href="/contact">联系我们</Link></li>
+            <li><Link href="/policy">隐私协议</Link></li>
+          </div>
+        </ul>
+      </nav>
 
       <main>
-        <div className={styles.logo}>
-          <img onClick={goHome} src="/images/chowislogo.webp" fetchpriority="high" />
-          <div>
-            {[
-              { id: '0', name: '首页' },
-              { id: '1', name: 'DermoPrime' },
-              { id: '2', name: 'DermoSmart' },
-              { id: '3', name: 'DermoPico' },
-              { id: '4', name: 'mySkin·Hair KIOSK' },
-              { id: '5', name: 'mySkin FAIN' },
-              { id: '6', name: 'DermoChoice' },
-              { id: '7', name: '联系我们' },
-              { id: '8', name: '隐私协议' },
-
-            ].map(product => (
-              <Link key={product.id} href={path(product.id)}>{product.name}</Link>
-            ))}
-          </div>
-        </div>
-
         {children}
-
       </main>
 
       <footer>
-
+        <div>Copyright © 2022 Chowis Co., Ltd. All rights reserved. <span>PRIVACY POLICY</span></div>
       </footer>
 
       <style jsx>{`
+        footer {
+          width: 100%;
+          height: 60px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: #002e5d;
+          color: white;
+          font-size: 14px;
+          padding: 0.8rem;
+        }
+
+        footer span {
+          margin-left: 2rem;
+        }
+
+        .navbar {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding: 20px;
+          width: 100%;
+        }
+      
+        /* NAVBAR MENU */
+        .menu {
+          display: flex;
+          gap: 1em;
+          font-size: 18px;
+        }
+        
+        .menu li {
+          padding: 5px 14px;
+        }
+        
+        /* DROPDOWN MENU */
+        .products {
+          position: relative; 
+        }
+        
+        .dropdown {
+          background-color: #e6e6e6;
+          padding: 1em 0;
+          position: absolute; /*WITH RESPECT TO PARENT*/
+          display: none;
+          top: 35px;
+        }
+        
+        .dropdown li + li {
+          margin-top: 10px;
+        }
+        
+        .dropdown li {
+          padding: 0.5em;
+          width: 8em;
+        }
+
+        .dropdown li:hover {
+          background-color: #ffffff91;
+          transition: 0.3s ease;
+        }
+        
+        .products:hover .dropdown {
+          display: block;
+        }
+
+        /* RESPONSIVE NAVBAR MENU STARTS */
+
+        /* CHECKBOX HACK */
+
+        input[type=checkbox] {
+          display: none;
+        } 
+
+        /* HAMBURGER MENU */
+        .hamburger {
+          display: none;
+          font-size: 24px;
+          user-select: none;
+        }
+
+        /* APPLYING MEDIA QUERIES */
+        @media (max-width: 768px) {
+        .navbar {
+          padding: 10px;
+        }
+        .navbar img {
+          height: 20px;
+        }
+        .hamburger {
+          font-size: 20px;
+        }
+        .menu {
+            display:none;
+            position: absolute;
+            background-color:#e2e2e2;
+            right: 0;
+            left: 0;
+            text-align: center;
+            padding: 16px 0;
+          }
+
+          .menu li:hover {
+            display: inline-block;
+            transition: 0.3s ease;
+          }
+
+          .menu li + li {
+            margin-top: 12px;
+          }
+
+          input[type=checkbox]:checked ~ .menu {
+            display: block;
+          }
+
+          .hamburger {
+            display: block;
+          }
+
+          .dropdown {
+            left: 50%;
+            top: 40px;
+            transform: translateX(5%);
+          }
+
+          .dropdown li:hover {
+            background-color: #4c9e9e;
+          }
+
+        }
+
         main {
           width: 100%;
-          padding: 1rem 0;
+          padding: 0 0 1rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
         }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
       `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
         * {
+          margin: 0;
+          padding: 0;
           box-sizing: border-box;
+        }
+        
+        a {
+          text-decoration: none;
+          color: black;
+          font-weight: 400;
+        }
+
+        a:hover {
+          color: rgb(29, 103, 205);
+          transition: 0.3s ease;
+        }
+        
+        li {
+          list-style: none;
         }
       `}</style>
     </div>
