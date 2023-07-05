fd80d24892bcc6c9d461a8399b8fca6c15e469e2
ibama2 <treeofstar@gmail.com>
2023-07-05 17:54:03 +0800

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -8,7 +8,7 @@ export default function Contact() {
         <Layout>
             <div className={styles.grid}>
                 <div className={styles.row + ' ' + contactStyles.contact}>
-                    <div className={styles.description}>
+                    <div className={styles.description + ' ' + styles.notMobile}>
                         <div className={contactStyles.head}>联系我们：</div>
 
                         <div>邮箱 chowis@chowis.com</div>
@@ -21,6 +21,17 @@ export default function Contact() {
 
                         <img className={contactStyles.code} />
                     </div>
+                </div>
+                <div className={styles.description + ' ' + contactStyles.isMobile}>
+                    <div className={contactStyles.head}>联系我们：</div>
+
+                    <div>邮箱 chowis@chowis.com</div>
+                    <div>微博 @ChowisOfficial</div>
+                    <div>小红书 @Chowis</div>
+                    <div>微信公众号@Chowis</div>
+                    <div>官网 www.chowis.com</div>
+                    <div>Skype ID Chowisofficial</div>
+                    <img className={contactStyles.code} />
                 </div>
             </div>
         </Layout>
