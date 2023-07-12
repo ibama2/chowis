8674c570e105b34362b597ec70c02fb0a786c0c9
ibama2 <treeofstar@gmail.com>
2023-07-12 11:45:29 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,11 @@ export default function Home() {
       <div className={styles.grid}>
         <div className={styles.row + ' ' + styles.pic1}>
           <div className={styles.description + ' ' + styles.notMobile}>
-            <div>基于AI的皮肤、毛发及头皮诊断系统</div>
-            <div>不论为了个人或商业活动，推荐选昭盈偲公司独有的多种基于AI的皮肤、头发及头皮诊断系统。</div>
+            <div>基于AI的皮肤、毛发及头皮诊断系统。不论为了个人或商业活动，推荐选昭盈偲公司独有的多种基于AI的皮肤、头发及头皮诊断系统。</div>
           </div>
         </div>
         <div className={styles.description + ' ' + styles.isMobile}>
-          <div>基于AI的皮肤、毛发及头皮诊断系统</div>
-          <div>不论为了个人或商业活动，推荐选昭盈偲公司独有的多种基于AI的皮肤、头发及头皮诊断系统。</div>
+          <div>基于AI的皮肤、毛发及头皮诊断系统。不论为了个人或商业活动，推荐选昭盈偲公司独有的多种基于AI的皮肤、头发及头皮诊断系统。</div>
         </div>
 
         <div className={styles.row + ' ' + styles.pic2}>
@@ -33,58 +31,58 @@ export default function Home() {
         <div className={styles.row + ' ' + styles.pic4}>
           <div className={styles.description + ' ' + styles.areas + ' ' + styles.notMobile}>
             <div className={styles.head}>欧洲</div>
-            <div>: 德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
+            <div>德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
 
             <div className={styles.head}>大洋洲</div>
-            <div>: 新西兰、澳大利亚、马来西亚、新加坡、菲律宾、印度尼西亚、圣诞岛、科科斯群岛和东帝汶
+            <div>新西兰、澳大利亚、马来西亚、新加坡、菲律宾、印度尼西亚、圣诞岛、科科斯群岛和东帝汶
             </div>
 
             <div className={styles.head}>欧洲</div>
-            <div>: 德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
+            <div>德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
 
             <div className={styles.head}>美洲</div>
-            <div>: 美国、加拿大和多米尼加共和国</div>
+            <div>美国、加拿大和多米尼加共和国</div>
 
             <div className={styles.head}>非洲</div>
-            <div>: 尼日利亚</div>
+            <div>尼日利亚</div>
 
             <div className={styles.head}>亚洲</div>
-            <div>: 大韩民国、缅甸、日本、中国、印度、乌兹别克斯坦、台湾、香港、伊朗</div>
+            <div>大韩民国、缅甸、日本、中国、印度、乌兹别克斯坦、台湾、香港、伊朗</div>
 
             <div className={styles.head}>中南美</div>
-            <div>: 阿根廷、厄瓜多尔、巴西、墨西哥</div>
+            <div>阿根廷、厄瓜多尔、巴西、墨西哥</div>
 
             <div className={styles.head}>俄罗斯</div>
-            <div>: 俄罗斯、哈萨克斯坦</div>
+            <div>俄罗斯、哈萨克斯坦</div>
 
           </div>
           <div className={styles.hint}>* 根据国家代码分类</div>
         </div>
         <div className={styles.description + ' ' + styles.areas + ' ' + styles.isMobile}>
           <div className={styles.head}>欧洲</div>
-          <div>: 德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
+          <div>德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
 
           <div className={styles.head}>大洋洲</div>
-          <div>: 新西兰、澳大利亚、马来西亚、新加坡、菲律宾、印度尼西亚、圣诞岛、科科斯群岛和东帝汶
+          <div>新西兰、澳大利亚、马来西亚、新加坡、菲律宾、印度尼西亚、圣诞岛、科科斯群岛和东帝汶
           </div>
 
           <div className={styles.head}>欧洲</div>
-          <div>: 德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
+          <div>德国、俄罗斯（独联体）、乌克兰、意大利、英国、法国、西班牙、奥地利、比利时、希腊</div>
 
           <div className={styles.head}>美洲</div>
-          <div>: 美国、加拿大和多米尼加共和国</div>
+          <div>美国、加拿大和多米尼加共和国</div>
 
           <div className={styles.head}>非洲</div>
-          <div>: 尼日利亚</div>
+          <div>尼日利亚</div>
 
           <div className={styles.head}>亚洲</div>
-          <div>: 大韩民国、缅甸、日本、中国、印度、乌兹别克斯坦、台湾、香港、伊朗</div>
+          <div>大韩民国、缅甸、日本、中国、印度、乌兹别克斯坦、台湾、香港、伊朗</div>
 
           <div className={styles.head}>中南美</div>
-          <div>: 阿根廷、厄瓜多尔、巴西、墨西哥</div>
+          <div>阿根廷、厄瓜多尔、巴西、墨西哥</div>
 
           <div className={styles.head}>俄罗斯</div>
-          <div>: 俄罗斯、哈萨克斯坦</div>
+          <div>俄罗斯、哈萨克斯坦</div>
 
         </div>
       </div>
